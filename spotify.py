31a4e50846be3dfb9d3f970f6a8a31cab53c3f3a
Adam Kapica <adam.kapica@gmail.com>
2020-12-14 03:38:02 +0100

    make code more simple

--- spotify.py
+++ spotify.py
@@ -17,31 +17,27 @@ def connect_to_spotify():
 
 def get_artists_id_list(spotify, seed):
     seed_id = spotify.search(q=seed, type='artist')['artists']['items'][0]['id']
-    artists, artists_db, queue = dict(), dict(), Queue()
+    artists_db, queue = dict(), Queue()
 
     queue.put(seed_id)
-    while queue.not_empty and len(artists) < DATABASE_SIZE:
+    while queue.not_empty and len(artists_db) < DATABASE_SIZE:
         artist = queue.get()
         related_artists = spotify.artist_related_artists(artist)['artists']
         for artist in related_artists:
-            if artist['id'] not in artists:
-                artists[artist['id']] = artist['name']
+            if artist['id'] not in artists_db:
+                artists_db[artist['id']] = Artist(artist['id'], artist['name'])
                 queue.put(artist['id'])
-
-    for id in artists:
-        artists_db[id] = Artist(id, artists[id])
 
     return artists_db
 
 
 def get_artist_tracks(spotify, artist):
-    track_ids, track_names = [], []
+    track_ids, track_names, result = [], [], []
 
     for item in spotify.artist_top_tracks(artist.spotify_id)['tracks'][:NUMBER_OF_TOP_TRACKS]:
         track_ids.append(item['id'])
         track_names.append(item['name'])
 
-    result = []
     for (name, features) in zip(track_names, spotify.audio_features(tracks=track_ids)):
         result.append(Track(name, **features))
 

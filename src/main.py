76131eb090154ecdaba08ad2eea6fa581297db1d
Adam Kapica <adam.kapica@gmail.com>
2021-01-05 22:33:13 +0100

    pr issue

--- src/main.py
+++ src/main.py
@@ -10,26 +10,26 @@ if __name__ == '__main__':
     spotify = connect_to_spotify()
 
     if os.path.exists(ARTISTS_DB_PATH_MAIN) and os.path.exists(ARTISTS_DB_PATH_SUPP):
-        artists_database_main = load_database(ARTISTS_DB_PATH_MAIN)
-        artists_database_supp = load_database(ARTISTS_DB_PATH_SUPP)
+        db_main = load_database(ARTISTS_DB_PATH_MAIN)
+        db_supp = load_database(ARTISTS_DB_PATH_SUPP)
     else:
-        artists_database_main = get_artists_id_list(spotify, seed="Kult")
-        artists_database_supp = add_related_and_unrelated_artists(spotify, artists_database_main)
+        db_main = get_artists_id_list(spotify, seed="Kult")
+        db_supp = add_related_and_unrelated_artists(spotify, db_main)
 
-        add_tracks(spotify, [artists_database_main, artists_database_supp])
-        calc_avg_track_features([artists_database_main, artists_database_supp])
+        add_tracks(spotify, [db_main, db_supp])
+        calc_avg_track_features([db_main, db_supp])
 
-        save_database(artists_database_main, ARTISTS_DB_PATH_MAIN)
-        save_database(artists_database_supp, ARTISTS_DB_PATH_SUPP)
+        save_database(db_main, ARTISTS_DB_PATH_MAIN)
+        save_database(db_supp, ARTISTS_DB_PATH_SUPP)
 
-    relevant_features = get_relevant_features(artists_database_main, artists_database_supp)
+    relevant_features = get_relevant_features(db_main, db_supp)
 
-    db_all_artists = dict(**artists_database_main, **artists_database_supp)
+    db_all = dict(**db_main, **db_supp)
 
     while True:
         artist_name = input('Enter artist name (or press Enter to exit): ')
         if artist_name:
-            recommendations = predict_recommendations(spotify, artist_name, db_all_artists, relevant_features)
+            recommendations = predict_recommendations(spotify, artist_name, db_all, relevant_features)
             for recommendation in recommendations:
                 print(recommendation)
         else:
